2258a6a44d0a4dd8319b43af5ee18754a092dcb8
Arvind <143075248+A-rvind@users.noreply.github.com>
2026-02-21 13:30:41 +0530

    Add Two Sum solution in 01-TwoSum.py
    Implement Two Sum solution using a dictionary to store indices.

--- 01-TwoSum.py
+++ 01-TwoSum.py
@@ -1,1 +1,17 @@
-#TwoSum
+#LeetCode #1 - Two Sum
+
+class Sol:
+  def twoSum(self, nums, target):
+    store = {}
+    for index, num in enumerate(nums):
+      search = target - num
+      if search in store:
+        return [index, store[search]]
+      else:
+        store[num] = index
+
+if __name__ == "__main__":
+  obj = Sol()
+  value = obj.twoSum([2,7,11,15],9)
+  print(value)
+
